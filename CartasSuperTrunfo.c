2f3c84fb13cdc7dda631d5b2f48b49308f9b1e54
JandsonMaia <jandsonmaia31@gmail.com>
2025-11-29 11:55:17 -0300

    Update CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -9,3 +9,17 @@ struct Carta {
     float pib;                  // PIB da cidade
     int pontosTuristicos;       // Quantidade de pontos turísticos
 };
+
+int main() {
+    struct Carta carta1, carta2;
+
+    printf("\n===== Cadastro da Carta 1 =====\n");
+
+    printf("Estado (A-H): ");
+    scanf(" %c", &carta1.estado);
+
+    printf("Código da Carta (ex: A01): ");
+    scanf("%s", carta1.codigo);
+
+    printf("Nome da Cidade: ");
+    scanf("%s", carta1.nomeCidade);
